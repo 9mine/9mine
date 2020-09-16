44fdb1462c08c4bad1910afaf6708d44441b2262
Oleksandr Dievri <o.dievri@gmail.com>
2020-09-16 20:58:35 +0300

    fix image tag

--- cdmod/on_joinplayer.lua
+++ cdmod/on_joinplayer.lua
@@ -9,9 +9,9 @@ minetest.register_on_joinplayer(function(player)
     player:set_pos({x = 0, y = 2, z = 0})
 
      local root = minetest.add_entity({x = 5, y = 7, z = 5}, "cdmod:directory")
-     root:set_nametag_attributes({text = "./"})
+     root:set_nametag_attributes({text = "jsonfs"})
      root:set_armor_groups({immortal = 0})
-     root:get_luaentity().path = "."
+     root:get_luaentity().path = "/mnt/jsonfs"
      root:set_acceleration({x = 0, y = -6, z = 0})
 
     local inventory = player.get_inventory(player)
