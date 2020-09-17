32e1eab4017a2dadf26d4ae906b7e1f1103704fd
Oleksandr Dievri <o.dievri@gmail.com>
2020-09-17 14:00:19 +0300

    add IP matching

--- cdmod/on_joinplayer.lua
+++ cdmod/on_joinplayer.lua
@@ -16,11 +16,3 @@ minetest.register_on_joinplayer(function(player)
 
 end)
 
-minetest.register_on_generated(function(minp, maxp, blockseed)
-    if minp.x < 0 and minp.y < 0 and minp.z < 0 then
-        local host_info = {type = "tcp", host = "inferno", port = 31000}
-        create_platform(0, 0, 0, 16, "h", nil, host_info, nil)
-
-    end
-end)
-
--- cdmod/traceroute.lua
+++ cdmod/traceroute.lua
@@ -1,12 +1,10 @@
 traceroute = function(host_info, path)
     local tcp = socket:tcp()
     local connection, err = tcp:connect(host_info["host"], host_info["port"])
-    if (err ~= nil) then
-        error("Connection error: " .. dump(err))
-    end
+    if (err ~= nil) then error("Connection error: " .. dump(err)) end
     local conn = np.attach(tcp, "dievri", "")
     local f = conn:newfid()
-    
+
     np:walk(conn.rootfid, f, path)
     conn:open(f, 0)
     local statistics = conn:stat(f)
@@ -18,5 +16,14 @@ traceroute = function(host_info, path)
     local file = io.open("test.txt", "w")
     file:write(content)
     file:close()
-    for line in io.lines("test.txt") do print(" THIS IS LINE " .. line) end
+    local route = {}
+    for line in io.lines("test.txt") do
+        if string.match(line, "^[ ]*[%d]+") ~= nil and
+            string.match(line, "%d+%.%d+%.%d+%.%d+") then
+            table.insert(route, string.match(line, "%d+%.%d+%.%d+%.%d+"))
+        end
+    end
+    for k, v in pairs(route) do
+        print(k .. " " .. v)
+    end
 end
