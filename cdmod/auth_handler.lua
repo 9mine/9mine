5217c6bc86b9d8accb30a8c6011817b5886177e9
Oleksandr Dievri <o.dievri@gmail.com>
2020-09-23 11:58:32 +0300

    update authentication logic

--- cdmod/auth_handler.lua
+++ cdmod/auth_handler.lua
@@ -1,65 +1,76 @@
 print("checkoint 1")
+
+minetest.register_on_authplayer(function(name, ip, is_success)
+    if is_success then authenticated = true end
+end)
+
 minetest.register_authentication_handler(
     {
         get_auth = function(name)
-            if authenticated == true then return end
-            -- GET PASSWORD HASH FROM LOCAL STORE
+            if authenticated == true then return cache end
+            -- check if password is in local storage 
             local success, password = pcall(read_file, "/tmp/" .. name)
-            print("checkoint -1")
-            if success == false then return end
-            -- GET AUTHENTICATION INFORMATION
+            -- if not, go to create_auth
+            if success == false then return nil end
+            -- try to authenticate with local password
             write_file("/tmp/cmdchan/export/cmd", "getauthinfo default auth " ..
                            name .. " " .. "'" .. password .. "'")
-            print("checkoint -2")
-            -- GET RESPONSE FROM AUTH CENTER 
             local response = read_file("/tmp/cmdchan/export/cmd")
-            print("RESPONSE FROM GETAUTHINFO .. " .. response)
-
-            print("checkoint -3")
-            print("PASSWORD FROM READFILE " .. password)
-            local new_password = password
-            local cache = {
-                password = new_password,
+            -- if not successfull, authentication fail
+            if string.match(response, "Auth fail") then
+                password = 'nil'
+            end
+            cache = {
+                password = password,
                 privileges = get_privileges(),
-                last_login = 1600767360
+                last_login = -1
             }
             return cache
         end,
 
         create_auth = function(name, password)
-            write_file("/tmp/cmdchan/export/cmd", "getauthinfo default auth " ..
-                           name .. " " .. "'" .. password .. "'")
-            print("checkoint 2")
-            local response = read_file("/tmp/cmdchan/export/cmd")
-            print("RESPONSE FROM GETAUTHINFO IN CREATE_AUTH .. " .. response)
-            if string.match(response, "Auth fail") then
-                print("FAILED TO AUTHENTICATE")
+            -- check if user with given name already exists
+            local success_remote, password_local =
+                pcall(read_file, "/n/client/users/" .. name)
+            -- if exists, try to authenticate with minetest client provided password
+            if success_remote == true then
+                write_file("/tmp/cmdchan/export/cmd",
+                           "getauthinfo default auth " .. name .. " " .. "'" ..
+                               password .. "'")
+                local response = read_file("/tmp/cmdchan/export/cmd")
+                -- if password is not match, authentication fails
+                if string.match(response, "Auth fail") then
+                    password = 'nil'
+                end
+                cache = {
+                    password = password,
+                    privileges = get_privileges(),
+                    last_login = -1
+                }
+                return cache
+                -- if user with not exists with a given name, create one. Authentication Ok.
+            else
                 write_file("/tmp/cmdchan/export/cmd",
                            "touch " .. "/tmp/" .. name)
-                write_file("/tmp/" .. name, "FAILED TO AUTHENTICATE")
-                return
+                read_file("/tmp/cmdchan/export/cmd")
+                write_file("/tmp/" .. name, password)
+                write_file("/n/client/tmp/cmdchan/export/newuser",
+                           name .. " " .. password)
+                local content =
+                    read_file("/n/client/tmp/cmdchan/export/newuser")
+                cache = {
+                    password = password,
+                    privileges = get_privileges(),
+                    last_login = -1
+                }
+                authenticated = true
+                return cache
             end
 
-            print("checkoint 4")
-            write_file("/tmp/cmdchan/export/cmd", "touch " .. "/tmp/" .. name)
-            print("checkoint 5")
-            read_file("/tmp/cmdchan/export/cmd")
-            print("checkoint 6")
-
-            write_file("/tmp/" .. name, password)
-            print("checkoint 7")
-            write_file("/n/client/tmp/cmdchan/export/newuser",
-                       name .. " " .. password)
-            print("checkoint 8")
-            local content = read_file("/n/client/tmp/cmdchan/export/newuser")
-            print("checkoint 9")
-            print("NEW USER IS: " .. content)
-
-            print("authentication created")
-            username = name
-            authenticated = true
         end,
-        set_password = function(name, password) print("setting password") end
+        set_password = function(name, password)
+            print("setting password failed")
+        end
     })
 
 minetest.register_on_leaveplayer(function(ObjectRef, timed_out)
