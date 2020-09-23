9641f893692464a263e8e953326346687a001bd3
Oleksandr Dievri <o.dievri@gmail.com>
2020-09-23 14:26:56 +0300

    use cmd from signer instead of direct access

--- cdmod/auth_handler.lua
+++ cdmod/auth_handler.lua
@@ -1,6 +1,3 @@
-print("checkoint 1")
-print(config.signer_addr)
-print(config.newuser_addr)
 minetest.register_on_authplayer(function(name, ip, is_success)
     if is_success then authenticated = true end
 end)
@@ -10,23 +7,23 @@ minetest.register_authentication_handler(
         get_auth = function(name)
             if authenticated == true then return cache end
             -- check if password is in local storage 
-            local success, password = pcall(read_file,
-                                            "/tmp/" .. name .. "/password")
+            local success, password = pcall(read_file, "/tmp/users/" .. name ..
+                                                "/password")
             -- if not, go to create_auth
             if success == false then return nil end
             -- try to authenticate with local password
-            print("PASSWORD IS " .. password)
-            write_file("/tmp/cmdchan/export/cmd", "getauthinfo default auth " ..
-                           name .. " " .. "'" .. password .. "'")
-            local response = read_file("/tmp/cmdchan/export/cmd")
+            write_file("/tmp/file2chan/cmd",
+                       "getauthinfo default auth " .. name .. " " .. "'" ..
+                           password .. "'")
+            local response = read_file("/tmp/file2chan/cmd")
             -- if not successfull, authentication fail
             if string.match(response, "Auth fail") then
                 password = 'nil'
             end
-            print("response is " .. response)
             local privs = {}
             if string.match(response, "Auth ok") then
-                privs = read_file("/n/client/users/" .. name .. "/privs")
+                write_file("/n/client/cmd", "cat /users/" .. name .. "/privs")
+                privs = read_file("/n/client/cmd")
             end
             cache = {
                 password = password,
@@ -38,15 +35,13 @@ minetest.register_authentication_handler(
 
         create_auth = function(name, password)
             -- check if user with given name already exists
-            local success_remote, password_local =
-                pcall(read_file, "/n/client/users/" .. name)
+            write_file("/n/client/cmd", "ls /users/" .. name)
+            local exists = read_file("/n/client/cmd")
             -- if exists, try to authenticate with minetest client provided password
-            if success_remote == true then
-                write_file("/tmp/cmdchan/export/cmd",
-                           "getauthinfo default auth " .. name .. " " .. "'" ..
-                               password .. "'")
-                local response = read_file("/tmp/cmdchan/export/cmd")
-                print(response)
+            if string.match(exists, "does not exists") then
+                write_file("/tmp/file2chan/cmd", "getauthinfo default auth " ..
+                               name .. " " .. "'" .. password .. "'")
+                local response = read_file("/tmp/file2chan/cmd")
                 -- if password is not match, authentication fails
                 if string.match(response, "Auth fail") then
                     password = 'nil'
@@ -55,15 +50,15 @@ minetest.register_authentication_handler(
                 return cache
                 -- if user with not exists with a given name, create one. Authentication Ok.
             else
-                write_file("/tmp/cmdchan/export/cmd",
-                           "mkdir " .. "/tmp/" .. name)
-                read_file("/tmp/cmdchan/export/cmd")
-                write_file("/tmp/cmdchan/export/cmd",
-                           "touch " .. "/tmp/" .. name .. "/password")
-                read_file("/tmp/cmdchan/export/cmd")
-                write_file("/tmp/" .. name .. "/password", password)
+                write_file("/tmp/file2chan/cmd",
+                           "mkdir " .. "/tmp/users/" .. name)
+                read_file("/tmp/file2chan/cmd")
+                write_file("/tmp/file2chan/cmd",
+                           "touch " .. "/tmp/users/" .. name .. "/password")
+                read_file("/tmp/file2chan/cmd")
+                write_file("/tmp/users/" .. name .. "/password", password)
                 local privs = minetest.settings:get("default_privs")
-                write_file("/n/client/tmp/cmdchan/export/newuser",
+                write_file("/n/client/newuser",
                            name .. " " .. password .. " " .. privs .. "")
                 cache = {
                     password = password,
