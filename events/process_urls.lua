8db54ddf1d8a65a3556fe04e56195255cc865f5a
Oleksandr Dievri <o.dievri@gmail.com>
2020-10-16 17:37:54 +0300

    add thumbnails caching

--- events/process_urls.lua
+++ events/process_urls.lua
@@ -1,22 +1,40 @@
 process_urls = function(name, entity)
     local urls = entity.content
     if not urls then return end
-    local thumbs = {}
-    for url in urls:gmatch("[^\n]+") do
-        local thumb = save_thumb(url)
-        table.insert(thumbs, thumb)
+    if entity.thumbs then
+        entity.object:set_properties({
+            automatic_rotate = 0,
+            nametag = "Query: " .. entity.req
+        })
+        show_thumbs(name, minetest.deserialize(entity.thumbs))
+    else
+
+        local thumbs = {}
+        for url in urls:gmatch("[^\n]+") do
+            local thumb = save_thumb(url)
+            table.insert(thumbs, thumb)
+        end
+        if #thumbs == 0 then
+            send_warning(name, "No video found")
+            return
+        end
+        local rtx = table.copy(thumbs)
+        table.shuffle(rtx)
+        entity.object:set_properties({
+            automatic_rotate = 0,
+            nametag = "Query: " .. entity.req,
+            textures = {
+                rtx[math.random(#rtx)] .. ".png",
+                rtx[math.random(#rtx)] .. ".png",
+                rtx[math.random(#rtx)] .. ".png",
+                rtx[math.random(#rtx)] .. ".png",
+                rtx[math.random(#rtx)] .. ".png",
+                rtx[math.random(#rtx)] .. ".png"
+            }
+        })
+
+        entity.thumbs = minetest.serialize(thumbs)
+        show_thumbs(name, thumbs)
     end
-    local rtx = table.copy(thumbs)
-    table.shuffle(rtx)
-    entity.object:set_properties({
-        automatic_rotate = 0,
-        nametag = "Query: " .. entity.req,
-        textures = {
-            rtx[math.random(#rtx)] .. ".png", rtx[math.random(#rtx)] .. ".png",
-            rtx[math.random(#rtx)] .. ".png", rtx[math.random(#rtx)] .. ".png",
-            rtx[math.random(#rtx)] .. ".png", rtx[math.random(#rtx)] .. ".png"
-        }
-    })
-    show_thumbs(name, thumbs)
 end
 
