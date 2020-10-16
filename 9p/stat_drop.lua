e9fe8e0fbaabecfc21d2a94a4892161f43f66e38
Oleksandr Dievri <o.dievri@gmail.com>
2020-10-16 22:32:54 +0300

    delete unused code

--- 9p/stat_drop.lua
+++
@@ -1,9 +0,0 @@
-stat_drop = function(a, path, name)
-    local conn = connections[name][a]
-    local f = conn:newfid()
-    conn:walk(conn.rootfid, f, path)
-    conn:open(f, 1)
-    local st = conn:stat(f)
-    st.length = 0
-    conn:wstat(f, st)
-end
