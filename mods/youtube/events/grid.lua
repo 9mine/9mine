770f71c68c83492a060ad123933d206255a0b035
Oleksandr Dievri <o.dievri@gmail.com>
2020-10-17 12:30:23 +0300

    fix typo; delete redundant files; update submodules

--- mods/youtube/events/grid.lua
+++ mods/youtube/events/grid.lua
@@ -3,6 +3,6 @@ youtube_grid = function(player, formname, fields)
     local ID, _ = next(fields)
     if ID == "quit" then return end
     add_video_item(ID, player)
-    send_warning(player_name, "Video ID " .. ID .. " added to inventory]")
+    send_warning(player_name, "Video ID " .. ID .. " added to inventory")
 end
 
