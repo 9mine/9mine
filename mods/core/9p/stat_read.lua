1a6cbb9be402d2b005c485318e50f27fddcb09d4
Oleksandr Dievri <o.dievri@gmail.com>
2020-10-21 11:18:04 +0300

    fix connection reference

--- mods/core/9p/stat_read.lua
+++ mods/core/9p/stat_read.lua
@@ -1,7 +1,7 @@
 stat_read = function(addr, path, player_name)
     local conn = connections[player_name][addr]
     local f = conn:newfid()
-    np:walk(conn.rootfid, f, path)
+    conn:walk(conn.rootfid, f, path)
     conn:open(f, 0)
     local st = conn:stat(f)
     conn:clunk(f)
