32a29e47941f309825720856e2f647fac9c7404c
Oleksandr Dievri <o.dievri@gmail.com>
2020-10-26 12:19:58 +0200

    change to nonblocking automount call

--- mods/core/funcs/automount.lua
+++ mods/core/funcs/automount.lua
@@ -1,6 +1,5 @@
 automount = function(player) 
-    fields = {}
-    fields.remote_address = core_conf:get("automount")
+    local fields = { remote_address = core_conf:get("automount"), spawn_attach = true }
     local addr, path, player = connect(player, fields)
     if addr and path and player then list_directory(addr, path, player) end
 end
--- mods/core/on_join/init_conn.lua
+++ mods/core/on_join/init_conn.lua
@@ -3,5 +3,5 @@
 minetest.register_on_joinplayer(function(player)
     local name = player:get_player_name()
     if not connections[name] then connections[name] = {} end
-    automount(player)
+    minetest.after(0.5, automount, player)
 end)
