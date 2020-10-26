e946c9a0d665eea222051eb599141d487e7b87fb
Oleksandr Dievri <o.dievri@gmail.com>
2020-10-26 12:07:48 +0200

    remove formname from function calls

--- mods/core/funcs/automount.lua
+++ mods/core/funcs/automount.lua
@@ -1,6 +1,6 @@
 automount = function(player) 
     fields = {}
     fields.remote_address = core_conf:get("automount")
-    local addr, path, player = connect(player, "", fields)
+    local addr, path, player = connect(player, fields)
     if addr and path and player then list_directory(addr, path, player) end
 end
