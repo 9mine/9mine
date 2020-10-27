cae53bc5de418cd00f8fbf97300e24735d973b2f
Oleksandr Dievri <o.dievri@gmail.com>
2020-10-27 15:23:53 +0200

    if env variable INFERNO_ADDRESS present use it as a connection string

--- mods/core/funcs/automount.lua
+++ mods/core/funcs/automount.lua
@@ -1,5 +1,5 @@
 automount = function(player) 
-    local fields = { remote_address = core_conf:get("automount"), spawn_attach = true }
+    local fields = { remote_address = os.getenv("INFERNO_ADDRESS") ~= "" and os.getenv("INFERNO_ADDRESS") or core_conf:get("inferno_address"), spawn_attach = true }
     local addr, path, player = connect(player, fields)
     if addr and path and player then list_directory(addr, path, player) end
 end
