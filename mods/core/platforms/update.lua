3300a59648891f8bc23f3c322d2f22323385b7b3
Oleksandr Dievri <o.dievri@gmail.com>
2020-11-04 23:40:57 +0200

    Protected call for error-prone function
    Issue #18

--- mods/core/platforms/update.lua
+++ mods/core/platforms/update.lua
@@ -4,22 +4,21 @@ plt.update = function(addr, path, player_name)
     local old_lst = plt_node and plt_node.listing
     local slots = plt_node and plt_node.slots
 
-    local new_lst = name_as_key(readdir(conn, path == "/" and "../" or path) or
-                                    {})
+    local new_lst = name_as_key(readdir(conn, path == "/" and "../" or path) or {})
     local prefix = path == "/" and path or path .. "/"
 
     for name, file in pairs(new_lst) do
         if old_lst[name] == nil then
             local i, slot = next(slots)
             table.remove(slots, i)
-            if (#slots / (plt_node.size * plt_node.size )) <  0.5 then 
+            if (#slots / (plt_node.size * plt_node.size)) < 0.5 then
                 local size = plt.get_size(plt_node.size * plt_node.size)
                 local root = nil
                 root, size = plt.resize(plt_node.root, slots, plt_node.size, size, addr, path)
                 plt_node.size = size
                 plt_node.root = root
             end
-            
+
             local hash = hex(addr .. prefix .. file.name)
             local file_node = graph:node(hash, {
                 stat = file,
@@ -28,8 +27,8 @@ plt.update = function(addr, path, player_name)
                 p = slot
             })
             graph:edge(plt_node, file_node)
-            old_lst[name] = file
             spawn_file(file, slot, addr, prefix .. file.name)
+            old_lst[name] = file
         end
     end
 
@@ -38,10 +37,10 @@ plt.update = function(addr, path, player_name)
             local hash = hex(addr .. prefix .. file.name)
             local file_node = graph:findnode(hash)
             table.insert(slots, file_node.p)
-            old_lst[name] = nil
-            remove_file(file_node.p)
+            pcall(remove_file, file_node.p)
             local edge_file_node = file_node:nextinput(nil)
             edge_file_node:delete()
+            old_lst[name] = nil
         end
     end
 
