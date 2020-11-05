c2deb85dc8c2d1bca8848d59342ace5ca38f26a0
Oleksandr Dievri <o.dievri@gmail.com>
2020-11-05 14:19:19 +0200

    double waiting time for automount

--- mods/core/on_join/init_conn.lua
+++ mods/core/on_join/init_conn.lua
@@ -1,5 +1,5 @@
 minetest.register_on_joinplayer(function(player)
     local name = player:get_player_name()
     if not connections[name] then connections[name] = {} end
-    minetest.after(0.5, automount, player)
+    minetest.after(1, automount, player)
 end)
