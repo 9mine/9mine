ca5d0f43d2f637d385c2e4ddd66fd3132c31571d
Oleksandr Dievri <o.dievri@gmail.com>
2020-11-06 17:38:10 +0200

    inject copied file into graph
    Issue #21

--- mods/core/funcs/mvcp/on_drop.lua
+++ mods/core/funcs/mvcp/on_drop.lua
@@ -5,12 +5,15 @@ on_drop = function(entity, addr, origin_path, player_name, name, command)
     local addr = meta:get_string("addr")
     local path = meta:get_string("path")
     local plt_node = graph:findnode(hex(addr .. path))
+
     local spawned = false
+    local slot = {}
     for k, v in pairs(plt_node.slots) do
         if v.x == node_pos.x and v.z == node_pos.z then
             minetest.chat_send_player(player_name, "Free slot found: " .. dump(v))
             spawned = true
             table.remove(plt_node.slots, k)
+            slot = v
             local rp = table.copy(v)
             rp.y = rp.y + 1
             entity:set_pos(rp)
@@ -21,6 +24,7 @@ on_drop = function(entity, addr, origin_path, player_name, name, command)
     if not spawned then
         minetest.chat_send_player(player_name, "No free slots found")
         entity:remove()
+        return
     end
 
     path = path == "/" and path or path .. "/"
@@ -35,10 +39,19 @@ on_drop = function(entity, addr, origin_path, player_name, name, command)
         if entity then
             entity:remove()
             minetest.chat_send_player(player_name, "File replaced")
+            file_node:delete()
         end
     end
+    
     local st = stat_read(addr, fullpath, player_name)
     plt_node.listing[name] = st
     entity:get_luaentity().stat = st
+    local new_file_node = graph:node(hex(addr .. fullpath), {
+        stat = st,
+        addr = addr,
+        path = fullpath,
+        p = slot
+    })
+    graph:edge(plt_node, new_file_node)
     minetest.chat_send_player(player_name, minetest.serialize(st))
 end
