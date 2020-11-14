d9381d28181e9f05a356d3acc83509b7c28051d8
Oleksandr Dievri <o.dievri@gmail.com>
2020-11-14 10:50:36 +0200

    As service node

---
+++ mods/core/nodes/service.lua
@@ -0,0 +1,49 @@
+ServiceNode = {
+    drawtype = "glasslike",
+    visual_scale = 1.0,
+    tiles = {"core_registry.png", "core_registry.png", "core_registry.png", "core_registry.png", "core_registry.png",
+             "core_registry.png"},
+    inventory_image = "core_registry.png",
+    use_texture_alpha = true,
+    stack_max = 1,
+    sunlight_propagates = false,
+    walkable = true,
+    pointable = true,
+    diggable = true,
+}
+
+function ServiceNode.on_drop(itemstack, dropper, pos)
+    local item_meta = itemstack:get_meta()
+    -- local name = item_meta:get_string("name")
+    itemstack:take_item(1)
+    local pos = dropper:get_pos()
+    local p = table.copy(pos)
+    local dir = dropper:get_look_dir()
+    dir.x = dir.x * 2.9
+    dir.y = dir.y * 2.9 + 2
+    dir.z = dir.z * 2.9
+    p.x = p.x + dir.x
+    p.y = p.y + dir.y
+    p.z = p.z + dir.z
+    local stat_entity = minetest.add_entity(p, "core:stat")
+    stat_entity:set_acceleration({
+        x = 0,
+        y = -9.81,
+        z = 0
+    })
+    -- minetest.after(2, CopyTool.on_drop, stat_entity, name, dropper:get_player_name(), "cp -r")
+    return itemstack
+end
+
+
+
+
+
+minetest.register_node("core:service_node", ServiceNode)
+
+minetest.register_on_joinplayer(function(player)
+    local inventory = player.get_inventory(player)
+    if not inventory:contains_item("main", "core:service_node") then
+        inventory:add_item("main", "core:service_node")
+    end
+end)
