c489ba87303c9075ba2755efa26b60abbcde427f
Oleksandr Dievri <o.dievri@gmail.com>
2020-11-17 02:03:24 +0200

    Mount service node when dropped

--- mods/core/nodes/service.lua
+++ mods/core/nodes/service.lua
@@ -1,3 +1,33 @@
+local function on_drop(itemstack, dropper, pos)
+    local item_meta = itemstack:get_meta()
+    local name = item_meta:get_string("service")
+    itemstack:take_item(1)
+    local pos = dropper:get_pos()
+    local p = table.copy(pos)
+    local dir = dropper:get_look_dir()
+    dir.x = dir.x * 2.9
+    dir.y = dir.y * 2.9 + 2
+    dir.z = dir.z * 2.9
+    p.x = p.x + dir.x
+    p.y = p.y + dir.y
+    p.z = p.z + dir.z
+    local stat_entity = minetest.add_entity(p, "core:stat")
+    stat_entity:set_acceleration({
+        x = 0,
+        y = -9.81,
+        z = 0
+    })
+    stat_entity:set_properties({
+        visual = "cube",
+        textures = {"core_service.png", "core_service.png", "core_service.png", "core_service.png", "core_service.png",
+                    "core_service.png"},
+        nametag = name
+    })
+    stat_entity:get_luaentity().service = name
+    minetest.after(2, ServiceNode.mount, stat_entity, dropper)
+    return itemstack
+end
+
 ServiceNode = {
     drawtype = "glasslike",
     visual_scale = 1.0,
@@ -10,7 +40,38 @@ ServiceNode = {
     walkable = true,
     pointable = true,
     diggable = true,
+    on_drop = on_drop
 }
 
+function ServiceNode.mount(entity, player)
+    local platform_string = common.get_platform_string_near(entity, player)
+    if not platform_string then
+        local item = ItemStack("core:service")
+        local service = item:get_meta()
+        service:set_string("service", entity:get_luaentity().service)
+        service:set_string("description", entity:get_luaentity().service)
+        local inventory = player.get_inventory(player)
+        inventory:add_item("main", item)
+        entity:remove()
+        return
+    end
+    minetest.chat_send_all(platform_string)
+
+    local cmdchan = platforms:get_cmdchan(platform_string)
+    local platform = platforms:get_platform(platform_string)
+    local platform_path = platform:get_path()
+
+    platform:set_external_handler_flag(true)
+    minetest.chat_send_all(cmdchan:execute("mount -A " .. entity:get_luaentity().service .. " " .. platform_path, "/"))
+    entity:set_acceleration({
+        x = 0,
+        y = 9,
+        z = 0
+    })
+    minetest.after(1.5, function(entity)
+        entity:remove()
+    end, entity)
+    platform:set_external_handler_flag(false)
+end
 
 minetest.register_node("core:service_node", ServiceNode)
