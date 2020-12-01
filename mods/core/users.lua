4946d9faa752c258fb4ce3a4abc5dabff188fa9b
Oleksandr Dievri <o.dievri@gmail.com>
2020-12-01 18:14:55 +0200

    Poll user from registry

--- mods/core/users.lua
+++ mods/core/users.lua
@@ -5,6 +5,21 @@ minetest.register_on_prejoinplayer(function(name, ip)
     end
 end)
 
+poll_regquery = function(name, counter, player, last_login)
+    if counter > 5 then
+        minetest.kick_player(name, "Error creating NS. Try again later")
+    end
+    counter = counter + 1
+    local user_addr = root_cmdchan:execute("ndb/regquery -n user " .. name):gsub("\n", "")
+    local response = root_cmdchan:execute("mount -A " .. user_addr .. " /n/" .. name)
+    if response == "" then
+        minetest.chat_send_player(name, user_addr .. " mounted")
+        minetest.after(2, spawn_root_platform, user_addr, player, last_login)
+    else
+        minetest.after(2, poll_regquery, name)
+    end
+end
+
 minetest.register_on_joinplayer(function(player, last_login)
     local name = player:get_player_name()
     local user_addr = root_cmdchan:execute("ndb/regquery -n user " .. name):gsub("\n", "")
@@ -14,16 +29,8 @@ minetest.register_on_joinplayer(function(player, last_login)
         minetest.after(2, spawn_root_platform, user_addr, player, last_login)
     else
         common.show_wait_notification(name, "Please, wait.\nThe namespace is creating.")
-        minetest.after(2, function(name)
-            local user_addr = root_cmdchan:execute("ndb/regquery -n user " .. name):gsub("\n", "")
-            local response = root_cmdchan:execute("mount -A " .. user_addr .. " /n/" .. name)
-            if response == "" then
-                minetest.chat_send_player(name, user_addr .. " mounted")
-                minetest.after(2, spawn_root_platform, user_addr, player, last_login)
-            else
-                minetest.kick_player(name, response .. "\n. Try again later")
-            end
-        end, name)
+        local counter = 1
+        minetest.after(2, poll_regquery, name, counter, player, last_login)
     end
 end)
 
