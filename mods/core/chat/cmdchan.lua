07029b6c3f2de0a4acfe51ee593b6774f6dbfe2d
Oleksandr Dievri <o.dievri@gmail.com>
2020-12-03 12:55:49 +0200

    Send message when no platform nearby found and prevent crash

--- mods/core/chat/cmdchan.lua
+++ mods/core/chat/cmdchan.lua
@@ -2,6 +2,9 @@ minetest.register_on_chat_message(function(player_name, message)
     local player = minetest.get_player_by_name(player_name)
     local player_graph = graphs:get_player_graph(player_name)
     local platform = player_graph:get_platform(common.get_platform_string(player))
+    if not platform then 
+        minetest.chat_send_player(player_name, "No platform found nearby")
+        return end
     local cmdchan = platform:get_cmdchan()
     if not cmdchan then
         return
