fe6c5ed1b09777fb203038978b750d434d1f04f5
Oleksandr Dievri <o.dievri@gmail.com>
2020-12-08 12:58:42 +0200

    Add execute to edit/write tools

--- mods/core/events/stat.lua
+++ mods/core/events/stat.lua
@@ -34,17 +34,17 @@ local write_rc = function(player, fields)
     else
         minetest.chat_send_player(player_name, "Editing file failed: " .. response)
     end
-    local execute_result = cmdchan:execute("cat " .. file_path)
+    local execute_result = cmdchan:execute("rc " .. file_path)
     minetest.show_formspec(player:get_player_name(), "core:file_content",
         table.concat({"formspec_version[3]", "size[13,13,false]", "textarea[0.5,0.5;12.0,12.0;;;",
                       minetest.formspec_escape(execute_result), "]"}, ""))
 end
 
 minetest.register_on_player_receive_fields(function(player, formname, fields)
-    if formname == "stat:write" or "stat:edit" then
+    if formname == "stat:write" or formname == "stat:edit" then
         write(player, fields)
     end
-    if formname == "stat:write_rc" then
+    if formname == "stat:write_rc" or formname == "stat:edit_rc" then
         write_rc(player, fields)
     end
 end)
--- mods/core/tools/write.lua
+++ mods/core/tools/write.lua
@@ -13,16 +13,17 @@ WriteTool = {
 function WriteTool.write(entity, player_name)
     local player_graph = graphs:get_player_graph(player_name)
     local directory_entry = player_graph:get_entry(entity.entry_string)
-    print(directory_entry.stat.name)
-    print(directory_entry.stat.name:match(".rc$"))
-    if directory_entry.stat.name:match(".rc$") then 
+    if directory_entry.stat.name:match(".rc$") then
         minetest.show_formspec(player_name, "stat:write_rc",
-        table.concat({"formspec_version[4]", "size[13,13,false]", "field[0,0;0,0;file_path;;" .. directory_entry.path .. "]",
-                      "textarea[0.5,0.5;12.0,10.6;content;;]", "button_exit[7,11.6;2.5,0.9;write;write]", "button[10,11.6;2.5,0.9;execute;execute]"}, ""))
+            table.concat({"formspec_version[4]", "size[13,13,false]",
+                          "field[0,0;0,0;file_path;;" .. directory_entry.path .. "]",
+                          "textarea[0.5,0.5;12.0,10.6;content;;]", "button_exit[7,11.6;2.5,0.9;write;write]",
+                          "button[10,11.6;2.5,0.9;execute;execute]"}, ""))
     else
-    minetest.show_formspec(player_name, "stat:write",
-        table.concat({"formspec_version[3]", "size[13,13,false]", "field[0,0;0,0;file_path;;" .. directory_entry.path .. "]",
-                      "textarea[0.5,0.5;12.0,10.6;content;;]", "button_exit[10,11.6;2.5,0.9;write;write]"}, ""))
+        minetest.show_formspec(player_name, "stat:write",
+            table.concat({"formspec_version[3]", "size[13,13,false]",
+                          "field[0,0;0,0;file_path;;" .. directory_entry.path .. "]",
+                          "textarea[0.5,0.5;12.0,10.6;content;;]", "button_exit[10,11.6;2.5,0.9;write;write]"}, ""))
     end
 end
 
