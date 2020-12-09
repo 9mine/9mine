c1d85e693b931a440ce65cb606db289848032d91
Oleksandr Dievri <o.dievri@gmail.com>
2020-12-09 22:03:02 +0200

    Show HUD path at top right corner with background
    Issue #71

--- mods/core/common.lua
+++ mods/core/common.lua
@@ -174,47 +174,101 @@ end
 
 -- Shows absolute path of the platform nearby 
 -- in right lower corner 
-function common.update_path_hud(player, id)
+function common.update_path_hud(player, id, addr_id, bg_id)
     local platform_string, error = common.get_platform_string(player)
-    if error then return end 
+    if error then
+        return
+    end
     local player_graph = graphs:get_player_graph(player:get_player_name())
     local platform = player_graph:get_platform(platform_string)
     local root_node = player_graph:get_root_node()
     if not platform_string then
         if id then
+            player:hud_remove(bg_id)
             player:hud_remove(id)
+            player:hud_remove(addr_id)
             id = nil
         end
     else
         if id then
-            player:hud_change(id, "text", platform.path .. "\n" .. platform.addr)
+            player:hud_change(bg_id, "number", (#platform.addr) > (#platform.path) and (#platform.addr) or (#platform.path))
+            player:hud_change(addr_id, "text", platform.addr)
+            player:hud_change(addr_id, "offset", {
+                x = -(#platform.addr * 10),
+                y = 20
+            })
+            player:hud_change(id, "text", platform.path)
             player:hud_change(id, "offset", {
-                x = -(#platform.path * 10) < -(#platform.addr * 10) and -(#platform.path * 10) or -(#platform.addr * 10),
-                y = -60
+                x = -(#platform.path * 10) - 5,
+                y = 60
             })
         else
             id = player:hud_add({
                 hud_elem_type = "text",
                 position = {
                     x = 1,
-                    y = 1
+                    y = 0
                 },
                 offset = {
-                    x = -(#platform.path * 10) < -(#platform.addr * 10) and -(#platform.path * 10) or -(#platform.addr * 10),
-                    y = -60
+                    x = -(#platform.path * 10) - 5,
+                    y = 60
                 },
-                text = platform.path .. "\n" .. platform.addr,
-                number = 0x228B22,
-                alignment = {
-                    x = 1,
-                    y = 1
-                }, 
+                text = platform.path,
+                number = 0x00FF00,
+                size = {
+                    x = 2
+                },
                 scale = {
                     x = 100,
                     y = 100
                 }
             })
+
+            addr_id = player:hud_add({
+                hud_elem_type = "text",
+                position = {
+                    x = 1,
+                    y = 0
+                },
+                offset = {
+                    x = -(#platform.addr * 10),
+                    y = 20
+                },
+                text = platform.addr,
+                number = 0x00FF00,
+                size = {
+                    x = 2
+                },
+                scale = {
+                    x = 100,
+                    y = 100
+                }
+            })
+
+            bg_id = player:hud_add({
+                hud_elem_type = "statbar",
+                z_index = -400,
+                direction = 1, 
+                number = (#platform.addr) > (#platform.path) and (#platform.addr) or (#platform.path),
+                position = {
+                    x = 1,
+                    y = 0
+                },
+                size = {
+                    x = 45, 
+                    y = 85
+                },
+                -- scale = {
+                --     x = 1,
+                --     y = 1
+                -- },
+                -- alignment = {
+                --     x = 1,
+                --     y = 0
+                -- },
+                text = "core_hud_bg.png"
+            })
         end
     end
-    minetest.after(1, common.update_path_hud, player, id)
+    minetest.after(1, common.update_path_hud, player, id, addr_id, bg_id)
 end
