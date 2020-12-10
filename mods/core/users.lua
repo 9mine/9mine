480df4dddea043ded06a01f811c5b136949e6f8c
Oleksandr Dievri <o.dievri@gmail.com>
2020-12-10 11:40:44 +0200

    Use registry index parser

--- mods/core/users.lua
+++ mods/core/users.lua
@@ -24,23 +24,24 @@ end
 
 draw_welcome_screen = function(player)
     local name = player:get_player_name()
-    local service_list = {}
     local registry = root_cmdchan:execute("cat /mnt/registry/index")
-    for server in registry:gmatch("[^\n]+") do
-        table.insert(service_list, server:match("[^ ]+"))
-    end
+    local services = common.parse_registry_index(registry)
     local service_string = ""
-    for index, service in pairs(service_list) do
-        service_string = service_string == "" and service or service_string .. "," .. service
+    for index, service in pairs(services) do
+        service_string = service_string == "" and service.service_addr or service_string .. "," .. service.service_addr
     end
     minetest.show_formspec(name, "core:global_registry", table.concat(
-        {"formspec_version[4]", "size[19,11,false]",
-         "hypertext[0.0,0.0;19.5,1;;<big><center>Select 9p service from the list <center><big>]",
-         "field[0.5,0.5;0,0;server_list;;", minetest.serialize(service_list), "]", "field[0.5,0.5;0,0;service_string;;",
-         service_string, "]", "tablecolumns[text]", "field[0.5,1;6.5,1;search;;]", "field_close_on_enter[search;false]",
-         "button[7,1;2.5,1;button_search;search]", "table[0.5,2.2;9,8.3;9p_server;", service_string, ";]",
-         "image[10,1;8.5,4;core_logo.png]",
-         "textarea[10,5.5;9,4.5;;;Welcome to 9mine Proof of Concept. This project aims to visualize 9p fileservers and interact with them in minecraft-style]"},
+        {   "formspec_version[4]", 
+            "size[19,11,false]",
+            "hypertext[0.0,0.0;19.5,1;;<big><center>Select 9p service from the list <center><big>]",
+            "field[0.5,0.5;0,0;services;;", minetest.formspec_escape(minetest.serialize(services)), "]", 
+            "field[0.5,0.5;0,0;service_string;;", service_string, "]", 
+            "tablecolumns[text]", "field[0.5,1;6.5,1;search;;]", 
+            "field_close_on_enter[search;false]",
+            "button[7,1;2.5,1;button_search;search]", 
+            "table[0.5,2.2;9,8.3;9p_server;", service_string, ";]",
+            "image[10,1;8.5,4;core_logo.png]",
+            "textarea[10,5.5;9,4.5;;;Welcome to 9mine Proof of Concept. This project aims to visualize 9p fileservers and interact with them in minecraft-style]"},
         ""))
 end
 
