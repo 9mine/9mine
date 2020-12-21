a4c769fa7930cfaf4fb6ea30b31047ca328be06d
Oleksandr Dievri <o.dievri@gmail.com>
2020-12-21 14:14:44 +0200

    Workaround for indentation
    Place non-whitespace character if front of new line with indentation
    for preventing hypertext trimming

--- mods/core/chat/cmdchan.lua
+++ mods/core/chat/cmdchan.lua
@@ -24,12 +24,15 @@ minetest.register_on_chat_message(function(player_name, message)
             common.add_ns_to_inventory(player, result)
         elseif message:match(" | man$") then
             message = message:gsub("| man", "")
-            local response = cmdchan:execute(message)
+            local response = cmdchan:execute(message)            
+                :gsub("\n ", "\n<style color=#00ffffff size=1>.</style>        ")
+                :gsub("%[", "\\%[")
+                :gsub("%]", "\\%]")
             local player_name = player:get_player_name()
             minetest.show_formspec(player_name, "core:man", table.concat(
-                {"formspec_version[4]", "size[13,13,false]",
-                 "hypertext[0.5, 0.5; 12.0, 11.0;;", minetest.formspec_escape(response), "]",
-                 "button_exit[10, 11.8;2.5,0.7;close;close]"}, ""))
+                {"formspec_version[4]", "size[15,13,false]",
+                 "hypertext[0.5, 0.5; 14.0, 11.0;;`<global background=yellow color=black><big>", response ,"</big>`]",
+                 "button_exit[12, 11.8;2.5,0.7;close;close]"}, ""))
         else
             local result = cmdchan:execute(message, path)
             minetest.chat_send_player(player_name, result .. "\n")
@@ -41,3 +44,34 @@ minetest.register_on_chat_message(function(player_name, message)
         return true
     end
 end)
+
+local man_event = function(player, formname, fields)
+    if formname == "core:man" then
+        if fields.quit then
+            return
+        end
+        local player_name = player:get_player_name()
+        local player = minetest.get_player_by_name(player_name)
+        local player_graph = graphs:get_player_graph(player_name)
+        local platform = player_graph:get_platform(common.get_platform_string(player))
+        if not platform then
+            minetest.chat_send_player(player_name, "No platform found nearby")
+            return true
+        end
+        local cmdchan = platform:get_cmdchan()
+        if not cmdchan then
+            return
+        end
+        local path = platform:get_path()
+        local response = cmdchan:execute("man man")    
+        :gsub("\n ", "\n<style color=#00ffffff size=1>.</style>        ")
+        :gsub("%[", "\\%[")
+        :gsub("%]", "\\%]")
+        minetest.show_formspec(player_name, "core:man", table.concat(
+            {"formspec_version[4]", "size[15,13,false]",
+             "hypertext[0.5, 0.5; 14.0, 11.0;;`<global background=yellow color=black><big>", response ,"</big>`]",
+             "button_exit[12, 11.8;2.5,0.7;close;close]"}, ""))
+    end
+end
+
+register.add_form_handler("core:man", man_event)
