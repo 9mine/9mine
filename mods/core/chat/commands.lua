e5b3afdc62598acdb8e9b6666cbdd64a1979108e
Oleksandr Dievri <o.dievri@gmail.com>
2021-01-05 15:02:32 +0200

    Prevent crash from cd command when no platform found

--- mods/core/chat/commands.lua
+++ mods/core/chat/commands.lua
@@ -3,6 +3,9 @@ minetest.register_chatcommand("cd", {
         local player = minetest.get_player_by_name(player_name)
         local player_graph = graphs:get_player_graph(player_name)
         local platform = player_graph:get_platform(common.get_platform_string(player))
+        if not platform then
+            return false, "No platform nearby"
+        end
         local path = platform:get_path()
         if not path_from_chat:match("^/") then
             path = path == "/" and path .. path_from_chat or path .. "/" .. path_from_chat
