621917235fbbc201ce26a1306c437c312ff4f3b4
Oleksandr Dievri <o.dievri@gmail.com>
2021-01-25 01:26:59 +0200

    add cmdchan docs

--- mods/core/cmdchan.lua
+++ mods/core/cmdchan.lua
@@ -1,10 +1,16 @@
+--- utilize cmdchan
 class 'cmdchan'
 
+--- constructor
+-- @tparam connection connection connection object
+-- @tparam string cmdchan_path path to the cmdchan
 function cmdchan:cmdchan(connection, cmdchan_path)
     self.connection = connection
     self.cmdchan_path = cmdchan_path
 end
 
+--- check if cmdchan is present at provided to costructor path
+-- @treturn bool present cmdchan or not
 function cmdchan:is_present()
     local conn = self.connection.conn
     if not conn then return end
@@ -15,6 +21,10 @@ function cmdchan:is_present()
     return result
 end
 
+--- write to the cmdchan
+-- @tparam string command command to be written to cmdchan
+-- @tparam string location path of platform from which command is executed
+-- @treturn nil
 function cmdchan:write(command, location)
     local conn = self.connection.conn
     local f = conn:newfid()
@@ -28,6 +38,9 @@ function cmdchan:write(command, location)
     conn:clunk(f)
 end
 
+--- read response from cmdchan
+-- @tparam string path path to file where response is located
+-- @treturn string response from cmdchan
 function cmdchan:read(path)
     local conn = self.connection.conn
     local f = conn:newfid()
@@ -48,6 +61,10 @@ function cmdchan:read(path)
     return response
 end
 
+--- execute (write and read) to cmdchan
+-- @tparam string command command to be written to cmdchan
+-- @tparam string location path of platform from which command is executed
+-- @treturn string response from cmdchan
 function cmdchan:execute(command, location)
     local tmp_file = "/n/cmdchan/cmdchan_output"
     command = command .. " > " .. tmp_file .. " >[2=1]"
@@ -55,6 +72,10 @@ function cmdchan:execute(command, location)
     return select(2, pcall(cmdchan.read, self, tmp_file))
 end
 
+--- show response from cmdchan in formspec
+-- @tparam string response response from cmdchan
+-- @tparam string player_name name of the player
+-- @treturn nil
 function cmdchan.show_response(response, player_name)
     minetest.show_formspec(player_name, "cmdchan:response",
                            table.concat({"formspec_version[3]", "size[13,13,false]",
