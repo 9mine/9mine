8d9f2e1f93c8fd1efef32d507ecdde34c8595d43
Oleksandr Dievri <o.dievri@gmail.com>
2021-02-08 19:03:06 +0200

    pass servers as arguments
    add server list

--- async/readsync.lua
+++ async/readsync.lua
@@ -6,11 +6,29 @@ pprint = require 'libs.pprint'
 np = require 'libs.9p'
 require 'libs.readdir'
 require 'mods.core.9p_over_tcp'
-
-local npio = np_over_tcp("tcp!9p.io!564")
-local ftrv = np_over_tcp("tcp!ftrv.se!564")
-npio:attach()
-ftrv:attach()
-pprint(readdir(npio.conn, "/"))
-pprint(readdir(ftrv.conn, "/"))
+local list_server = {"tcp!registry.demo.metacoma.io!30099",
+"tcp!registry.demo.metacoma.io!30100",
+"tcp!registry.9gridchan.org!6675",
+"tcp!registry.9gridchan.org!7675",
+"tcp!docs.a-b.xyz!909",
+"tcp!oat.nine.sirjofri.de!564",
+"tcp!ftrv.se!564",
+"tcp!9p.io!564",
+"tcp!postnix.pw!564",
+"tcp!postnix.pw!564",
+"tcp!postnix.pw!626",
+"tcp!postnix.pw!626",
+"tcp!postnix.pw!465",
+"tcp!contrib.9front.org!564",
+"tcp!kamalatta.ddnss.de!999",
+"tcp!kamalatta.ddnss.de!9997"}
+math.randomseed(os.time())
+local first = np_over_tcp(arg[1] or list_server[math.random(#list_server)])
+local second = np_over_tcp(arg[2] or list_server[math.random(#list_server)])
+first:attach()
+second:attach()
+pprint(first)
+readdir(first.conn, "/")
+pprint(second)
+readdir(second.conn, "/")
 
