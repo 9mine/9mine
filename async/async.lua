f3d5ba9952bf10b0b338e0a5a3eeb09a9a4f050e
Oleksandr Dievri <o.dievri@gmail.com>
2021-02-10 15:35:39 +0200

    cqueue wrap

--- async/async.lua
+++ async/async.lua
@@ -1,11 +1,19 @@
 local  cqueues = require"cqueues"
 local  socket = require"cqueues.socket"
-
+local cq = cqueues.new()
 local sct = socket.connect("localhost", 9000)
 sct:settimeout(1)
-local str = "world\n"
-local byte_count, error_code = sct:send(str, 1, str:len())
-print(byte_count, error_code)
 
-local response, error_code = sct:recv("*l")
-print(response, error_code)
+while true do
+cq:wrap(function()
+    local str = "world\n"
+	local _, error_code = sct:send(str, 1, str:len())
+	if not error_code then print("sent") end
+	local response
+	repeat
+	response = sct:recv("*l")
+	until response
+	print(response)
+end)
+	cq:step()
+end
