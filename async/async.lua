4469e0eab9ae33e984322afd7fff1ba27d7d91e0
Oleksandr Dievri <o.dievri@gmail.com>
2021-02-11 11:36:04 +0200

    poll sockets for events

--- async/async.lua
+++ async/async.lua
@@ -1,19 +1,36 @@
 local  cqueues = require"cqueues"
 local  socket = require"cqueues.socket"
-local cq = cqueues.new()
-local sct = socket.connect("localhost", 9000)
-sct:settimeout(1)
+local pprint = require 'libs.pprint'
+local sct1 = socket.connect("localhost", 9000)
+sct1:settimeout(0)
+local sct2 = socket.connect("localhost", 2701)
+sct2:settimeout(0)
+
+function socket_send(socket, message)
+	socket:send(message, 1, message:len())
+end
+
+function socket_recv(socket)
+	local response
+	response = socket:recv("*l")
+	return response
+end
 
 while true do
-cq:wrap(function()
-    local str = "world\n"
-	local _, error_code = sct:send(str, 1, str:len())
-	if not error_code then print("sent") end
-	local response
-	repeat
-	response = sct:recv("*l")
-	until response
-	print(response)
-end)
-	cq:step()
+	
+local ready = {assert (cqueues.poll(sct1, sct2, 1)) }
+if type(ready[1]) ~= "number" then
+for i = 1, #ready do
+	if ready[i]:events() == "w" then
+	socket_send(ready[i], "world\n")
+	else
+		local response = socket_recv(ready[i])
+		if response == nil then 
+			socket_send(ready[i], "world\n")
+		end
+		print(response)
+		pprint(ready[i]:events())
+	end
+end
+end
 end
