0ea65e37758b8ca5b6739917a4f8e11d54bd2779
Oleksandr Dievri <o.dievri@gmail.com>
2021-02-12 13:00:50 +0200

    fetch data on globalstep

--- mods/core/async.lua
+++ mods/core/async.lua
@@ -1,41 +1,54 @@
 local cqueues = require "cqueues"
 local socket = require "cqueues.socket"
-local sct1 = socket.connect("localhost", 9000)
-sct1:settimeout(0)
-local sct2 = socket.connect("localhost", 2701)
+local serpent = require 'serpent'
+local sct2 = socket.connect {
+    host = "localhost",
+    port = 9000,
+    nonblock = true
+
+}
 sct2:settimeout(0)
 
-function socket_send(socket, message)
-    socket:send(message, 1, message:len())
-end
-
-function socket_recv(socket)
-    local response
-    response = socket:recv("*l")
-    return response
-end
+local prev_in_mode, prev_out_mode = sct2:setmode("b", "b")
+print(prev_in_mode, prev_out_mode)
+local prev_in_buf, prev_out_buf = sct2:setbufsiz(1024, 1024)
+print(prev_in_buf, prev_out_buf)
+local full_resp = ""
 
 local timer = 0
 minetest.register_globalstep(function(dtime)
     timer = timer + dtime;
     if timer >= 2 then
-        local ready = {assert(cqueues.poll(sct1, sct2, 1))}
+        local ready = {assert(cqueues.poll(sct2, 0))}
         if type(ready[1]) ~= "number" then
             for i = 1, #ready do
-                if ready[i]:events() == "w" then
-                    socket_send(ready[i], "world\n")
-                else
-                    local response = socket_recv(ready[i])
-                    if response == nil then
-                        socket_send(ready[i], "world\n")
-					else
-						minetest.chat_send_all("Response from sockets: " .. response)
-					end
+                if ready[i] == 0 then
+                    break
                 end
+                print("events before: ", serpent.block(ready[i]:events()))
+                if ready[i]:events() == "w" or ready[i]:events() == "r" then
+                    local to_send = string.rep("world", 10000) .. "\n"
+                    minetest.chat_send_all("Length of sent data: " .. string.len(to_send))
+                    local sent = ready[i]:xwrite(to_send)
+                elseif ready[i]:events() ~= "w" then
+                    local response, error = ready[i]:recv("*l")
+                    minetest.chat_send_all("Receive error code is: " .. tostring(error))
+                    if response ~= nil then
+                        full_resp = full_resp .. response
+                    end
+                    minetest.chat_send_all("Full length: " .. string.len(full_resp))
+                    minetest.chat_send_all("events after: ", serpent.block(ready[i]:events()))
+                    if ready[i]:events() == "r" then
+                        minetest.chat_send_all(full_resp)
+                        full_resp = ""
+                    end
+                end
+
+                -- Read Response From servers each 2 seconds
+
+                timer = 0
             end
         end
-        -- Read Response From servers each 2 seconds
-        
-        timer = 0
     end
 end)
+
