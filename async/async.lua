31ae22e825d876f8b3bd029f06135281b0b32af9
Oleksandr Dievri <o.dievri@gmail.com>
2021-02-12 12:30:05 +0200

    receive data in multiple steps

--- async/async.lua
+++ async/async.lua
@@ -1,36 +1,46 @@
-local  cqueues = require"cqueues"
-local  socket = require"cqueues.socket"
+local cqueues = require "cqueues"
+local socket = require "cqueues.socket"
 local pprint = require 'libs.pprint'
-local sct1 = socket.connect("localhost", 9000)
-sct1:settimeout(0)
-local sct2 = socket.connect("localhost", 2701)
+local serpent = require 'serpent'
+local data = require 'data'
+
+local sct2 = socket.connect {
+    host = "localhost",
+    port = 9000,
+    nonblock = true
+
+}
 sct2:settimeout(0)
 
-function socket_send(socket, message)
-	socket:send(message, 1, message:len())
+local prev_in_mode, prev_out_mode = sct2:setmode("b", "b")
+print(prev_in_mode, prev_out_mode)
+local prev_in_buf, prev_out_buf = sct2:setbufsiz(1024, 1024)
+print(prev_in_buf, prev_out_buf)
+local full_resp = ""
+while true do 
+    local ready = {assert(cqueues.poll(sct2, 0))}
+    if type(ready[1]) ~= "number" then
+    for i = 1, #ready do
+        if ready[i] == 0 then break end
+		print("events before: ", serpent.block(ready[i]:events()))
+        if ready[i]:events() == "w" or ready[i]:events() == "r" then
+            local to_send = string.rep("world", 10000) .. "\n"
+            print("Length of sent data: " .. string.len(to_send))
+            local sent = ready[i]:xwrite(to_send)
+        elseif ready[i]:events() ~= "w" then
+            local response, error = ready[i]:recv("*l")
+            print("Receive error code is: " .. tostring(error))
+            if response ~= nil then 
+                full_resp = full_resp .. response 
+            end
+            print("Full length: " .. string.len(full_resp))
+            print("events after: ", serpent.block(ready[i]:events()))
+            if ready[i]:events() == "r" then
+               -- minetest.chat_send_all(full_resp)
+                full_resp = ""
+            end
+        end
+    end
 end
-
-function socket_recv(socket)
-	local response
-	response = socket:recv("*l")
-	return response
-end
-
-while true do
-	
-local ready = {assert (cqueues.poll(sct1, sct2, 1)) }
-if type(ready[1]) ~= "number" then
-for i = 1, #ready do
-	if ready[i]:events() == "w" then
-	socket_send(ready[i], "world\n")
-	else
-		local response = socket_recv(ready[i])
-		if response == nil then 
-			socket_send(ready[i], "world\n")
-		end
-		print(response)
-		pprint(ready[i]:events())
-	end
-end
-end
+os.execute("sleep 1")
 end
